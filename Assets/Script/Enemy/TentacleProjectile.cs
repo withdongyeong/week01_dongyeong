3cb01f6faf69ef7ab5d3e796f4f91b4e785bf318
Mireene-Unity <dhalsghkdxld@gmail.com>
2025-03-05 22:25:32 +0900

    Kraken
    Boss

--- Assets/Script/Enemy/TentacleProjectile.cs
+++ Assets/Script/Enemy/TentacleProjectile.cs
@@ -5,18 +5,16 @@ using static UnityEngine.GraphicsBuffer;
 
 public class TentacleProjectile : MonoBehaviour
 {
-    public float speed = 6f; // �̵� �ӵ�
+    public float speed = 6f; // 이동 속도
     public float returnSpeed = 6f;
-    public Vector3 targetPosition; // Ŭ���� ��ġ
-    public Vector3 startPosition; // ���� ��ġ 
-    public GameObject tail;    // �� ���� �κ�
+    public Vector3 targetPosition; // 클릭한 위치
+    public Vector3 startPosition; // 시작 위치 
+    public GameObject tail;    // 끈 연결 부분
 
-    public GameObject Kraken;
+    public float acceleration = 2f; // 가속도
+    private float currentSpeed = 0f; // 현재 속도
 
-    public float acceleration = 2f; // ���ӵ�
-    private float currentSpeed = 0f; // ���� �ӵ�
-
-    private Vector3 originalScale; // ���� ũ��
+    private Vector3 originalScale; // 원래 크기
     private bool isMoving = false;
     private bool isReturn = false;
     private float reloadingTime;
@@ -24,22 +22,23 @@ public class TentacleProjectile : MonoBehaviour
 
 
     GameObject enemy;
-    GameObject ownerObj;
+    GameObject bossObj;
+    GameObject playerObj;
 
     Transform[] points = new Transform[2];
 
 
     private void Awake()
     {
-        ownerObj = gameObject;
-        _playerAttack = ownerObj.GetComponent<Bbb10311031_PlayerAttack>();
+        bossObj = GameObject.FindGameObjectWithTag("Boss");
+        playerObj = GameObject.FindGameObjectWithTag("Player");
     }
 
     void Start()
     {
         startPosition = transform.position;
-        targetPosition = new Vector3(targetPosition.x, targetPosition.y, 0f); // 3D ��ǥ�� 2D�� ����
-        returnSpeed = returnSpeed * StateManager.Instance.ReloadingTime(); // ���ƿ��� �ӵ�
+        targetPosition = new Vector3(playerObj.transform.position.x, playerObj.transform.position.y, 0f); // 3D 좌표값 2D로 변경
+        returnSpeed = returnSpeed * StateManager.Instance.ReloadingTime(); // 돌아오는 속도
         SetTail();
 
         isMoving = true;
@@ -47,8 +46,9 @@ public class TentacleProjectile : MonoBehaviour
 
     void SetTail()
     {
-        points[0] = Kraken.transform;
+        points[0] = bossObj.transform;
         points[1] = gameObject.transform;
+
         tail.GetComponent<LineController>().SetUpLine(points);
     }
 
@@ -59,12 +59,12 @@ public class TentacleProjectile : MonoBehaviour
 
         if (isMoving)
         {
-            transform.up = (targetPosition - startPosition).normalized; // �ۻ� ��ü ����
+            transform.up = (targetPosition - startPosition).normalized; // 작살 물체 방향
 
-            // �̵�
+            // 이동
             transform.position = Vector3.Lerp(transform.position, targetPosition, speed * Time.deltaTime);
 
-            // ������ �����ϸ� ����
+            // 목적지 도착하면 멈춤
             if (Vector3.Distance(transform.position, targetPosition) <= 0.1f)
             {
                 if (enemy != null)
@@ -76,7 +76,7 @@ public class TentacleProjectile : MonoBehaviour
                 {
                     //SoundManager.instance.PlaySFX("SmallCanon");
                 }
-                isReturn = true; // ������ ��������
+                isReturn = true; // 끝까지 도달했음
                 isMoving = false;
                 GetComponent<CapsuleCollider2D>().enabled = false;
                 //Destroy(gameObject);
@@ -84,15 +84,11 @@ public class TentacleProjectile : MonoBehaviour
         }
         else if (isReturn)
         {
-            transform.up = Vector3.Lerp(transform.up, (targetPosition - ownerObj.transform.position).normalized, Time.deltaTime);
-            transform.position = Vector3.MoveTowards(transform.position, ownerObj.transform.position, returnSpeed * Time.deltaTime);
+            transform.up = Vector3.Lerp(transform.up, (targetPosition - bossObj.transform.position).normalized, Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, bossObj.transform.position, returnSpeed * Time.deltaTime);
 
-
-            SpearRotation();
-
-            if (Vector3.Distance(transform.position, ownerObj.transform.position) < 0.1f)
+            if (Vector3.Distance(transform.position, bossObj.transform.position) < 0.1f)
             {
-                _playerAttack.AttackCountUp();
                 Destroy(gameObject);
             }
         }
@@ -100,12 +96,7 @@ public class TentacleProjectile : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Enemy") && isMoving) // ���� �浹�ϸ�
-        {
-            Destroy(other.gameObject);
-            ReturnStart();
-        }
-        if (other.CompareTag("Obstacle") && isMoving) // ��ֹ��� �浹�ϸ�
+        if (other.CompareTag("Player") && isMoving) // 장애물과 충돌하면
         {
             ReturnStart();
         }
@@ -113,25 +104,12 @@ public class TentacleProjectile : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Enemy") && isMoving) // ���� �浹�ϸ�
-        {
-            Destroy(collision.gameObject);
-            ReturnStart();
-        }
-        if (collision.gameObject.CompareTag("Obstacle") && isMoving) // ��ֹ��� �浹�ϸ�
+        if (collision.gameObject.CompareTag("Player") && isMoving) // 장애물과 충돌하면
         {
             ReturnStart();
         }
     }
-    
-    void SpearRotation()
-    {
-        Vector2 direction = transform.position - ownerObj.transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
-    }
-
-    // ���ƿ��� ������ �� �浹 ���� �ȳ����� ����
+    // 돌아오기 시작할 때 충돌 판정 안나도록 설정
     void ReturnStart()
     {
         isMoving = false;
