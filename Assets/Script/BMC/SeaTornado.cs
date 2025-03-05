52112b696fa780a682ac8faf5046e9f64a4a0ab3
bokob <bmc0520@naver.com>
2025-03-05 10:41:59 +0900

    [FIX] 파괴 테스트 코드

--- Assets/Script/BMC/SeaTornado.cs
+++ Assets/Script/BMC/SeaTornado.cs
@@ -10,6 +10,8 @@ public class SeaTornado : Attraction
     [SerializeField] float angle = 5f;
     [SerializeField] float intervalLine = 0.1f;
 
+    [SerializeField] float lifeTime = 5f;
+
     private void Update()
     {
         //if (Vector3.Distance(trail.transform.position, transform.position) < 0.5f)
@@ -23,9 +25,9 @@ public class SeaTornado : Attraction
         trail.transform.RotateAround(transform.position, Vector3.forward, angle);
         trail.position = Vector3.Slerp(trail.transform.position, transform.position, intervalLine * Time.deltaTime);
 
-        
-
         transform.Translate(0, 0, 0.1f * Time.deltaTime);
+
+        Destory();
     }
 
     // 배를 회오리 중심으로 끌어당기는 방향 반환
@@ -35,4 +37,9 @@ public class SeaTornado : Attraction
         return (transform.position - applyObject.transform.position).normalized;
     }
 
+    void Destory()
+    {
+        if (transform.position.z >= 1f)
+            Debug.Log("토네이도 소멸!");
+    }
 }
